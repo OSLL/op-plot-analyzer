1fdb26775c0e97244077016b037007cd34f0c9d5
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-02-25 08:32:24 +0300

    Заготовка приложения

---
+++ OnPlotAnalyzer.java
@@ -0,0 +1,11 @@
+class OnPlotAnalyzer{
+	public static void main(String[] args) {
+		int i = 0;
+		while(i != args.length) {
+            		if (args[i].equals("-about")){
+				System.out.println("This a kino analyser!");
+			}
+			i++;
+        	}
+	}
+}
